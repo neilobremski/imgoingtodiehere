262b324c7bd4025f4db41c6407a9256996993ece
NeilO <masta@gibdon.com>
2024-11-10 20:39:29 +0000

    restructure

--- imgoingtodiehere.js
+++ imgoingtodiehere.js
@@ -6,10 +6,42 @@ function loadCss(href) {
   document.head.appendChild(link)
 }
 
-// configure onload event
+function loadJavascript(href) {
+  const script = document.createElement('script')
+  script.src = href
+  document.head.appendChild(script)
+}
+
+// create a `tag` element with `contents` assigned properties and append `children`
+function make(tag, contents, children, attributes) {
+  const element = document.createElement(tag)
+  Object.assign(element, contents)
+  for (const child of (children || [])) {
+    element.appendChild(child)
+  }
+  for (const attribute of Object.keys(attributes || {})) {
+    element.setAttribute(attribute, attributes[attribute])
+  }
+  return element
+}
+
 window.onload = function() {
-  const div = document.createElement("div")
-  div.innerText = "DOM is fully loaded"
-  document.documentElement.appendChild(div)
-  loadCss('/imgoingtodiehere.css')
+  const body = document.querySelector("body")
+
+  const footerHtml = `
+  <hr>
+  <copyright><a href="https://imgoingtodiehere.com">imgoingtodiehere.com</a> &copy; 2024++ by Greg Havener and Neil C. Obremski</copyright>
+  `
+
+  const mainContainer = make("main", {className: "container"}, [
+    ...Array.from(body.children),
+    make("footer", {innerHTML: footerHtml})
+  ])
+
+  body.appendChild(mainContainer)
+
+  loadCss("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css")
+  loadCss("/imgoingtodiehere.css")
+
+  loadJavascript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js")
 }
